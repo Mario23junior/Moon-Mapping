a90fcbe62349e0729494896e198f4d4f4a71166f
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-07-25 05:30:52 -0300

    Criando interface do tipo moon

---
+++ src/app/moons/models/moon.ts
@@ -0,0 +1,13 @@
+export interface Moon {
+    _id: number,
+    name: String,
+    planetaMae: string
+    semieixoMaior: String,
+    pressaoAtmosferica: String,
+    massa: number,
+    periodoDeRotacao: String,
+    periodoOrbital: String,
+    inclinacao: string,
+    velocidadeOrbitalMedia: number
+
+}
